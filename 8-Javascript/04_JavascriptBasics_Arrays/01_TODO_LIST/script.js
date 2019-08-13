4ae99589077bb8ee44eab588d2df9578fa76cf0d
Ricardo Pereira <ricardojfpereira99@gmail.com>
2019-08-13 17:16:05 +0100

    Improvements and refactor the TODO LIST with Javascript

--- 8-Javascript/04_JavascriptBasics_Arrays/01_TODO_LIST/script.js
+++ 8-Javascript/04_JavascriptBasics_Arrays/01_TODO_LIST/script.js
@@ -6,25 +6,50 @@ window.setTimeout(function () {
 
 
 
-  while (input !== "quit") {
 
+  while (input !== "quit") {
 
     // handle input
     if (input === "list") {
-      console.log(todos);
+      listTodos();
     } else if (input === "new") {
-      // ask for new todo
-      var newTodo = prompt("Enter new todo");
-      // add to todos array
-      todos.push(newTodo);
+      addTodo();
+    } else if (input === "delete") {
+      deleteTodo();
     }
 
-
+    // ask user an action
     input = prompt("What would you like to do?");
 
   }
 
   console.log("Okay! You quit the APP.......");
 
+
+
+
+  function listTodos() {
+    console.log("*****************");
+    todos.forEach(function (todo, index, array) {
+      console.log(index + ": " + todo);
+    });
+    console.log("*****************");
+  }
+
+  function addTodo() {
+    // ask for new todo
+    var newTodo = prompt("Enter new todo");
+    // add to todos array
+    todos.push(newTodo);
+    console.log("ADDED TODO....");
+  }
+
+  function deleteTodo() {
+    // ask for index to be deleted
+    var index = prompt("Enter index of TODO to delete");
+    // delete the todo via index provided
+    todos.splice(index, 1);
+    console.log("DELETED TODO....");
+  }
 
 }, 500);
