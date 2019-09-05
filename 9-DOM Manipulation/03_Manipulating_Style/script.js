1cfd86cb8964c14fe2c6b53af4e33089e4191dd7
Ricardo Pereira <ricardojfpereira99@gmail.com>
2019-09-05 18:47:05 +0100

    Manipulating Style

---
+++ 9-DOM Manipulation/03_Manipulating_Style/script.js
@@ -0,0 +1,14 @@
+var h1 = document.querySelector("h1");
+
+h1.style.color = "yellow";
+h1.style.border = "5px solid pink";
+
+
+var p = document.querySelector("p");
+
+p.classList.add("big");
+// OR
+// p.classList.toggle("big"); IF IS ON IT TURNS THE CLASS OFF, otherwise DO THE REVERSE. This is like a switch
+
+
+// p.classList.remove("big");
