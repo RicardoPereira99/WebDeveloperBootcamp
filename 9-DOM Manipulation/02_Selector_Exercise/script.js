bdd33c2ffc4c473160608234a251b3240889c669
Ricardo Pereira <ricardojfpereira99@gmail.com>
2019-09-05 18:32:24 +0100

    Selector Exercise

---
+++ 9-DOM Manipulation/02_Selector_Exercise/script.js
@@ -0,0 +1,7 @@
+var tagOne = document.getElementById("first");
+
+var tagTwo = document.getElementsByClassName("special")[0];
+
+var tagThree = document.getElementsByTagName("p")[0];
+
+var tagFour = document.querySelector("#first");
