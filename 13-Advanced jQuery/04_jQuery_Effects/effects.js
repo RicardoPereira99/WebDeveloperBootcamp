9e15b0b66e42d342daa32de2712e97c430605c25
Ricardo Pereira <ricardojfpereira99@gmail.com>
2020-03-07 12:07:13 +0000

    jQuery Effects

---
+++ 13-Advanced jQuery/04_jQuery_Effects/effects.js
@@ -0,0 +1,37 @@
+// $("button").on("click", function(){
+//     $("div").fadeOut(1000, function(){
+//         $(this).remove();
+//         console.log("Fade Completed");
+//     });
+    
+// });
+
+
+// $("button").on("click", function(){
+//     $("div").fadeIn(1000, function(){
+//         console.log("Fade Completed");
+//     });
+    
+// });
+
+// $("button").on("click", function(){
+//     $("div").fadeToggle(1000, function(){
+//         console.log("Fade Completed");
+//     });
+    
+// });
+
+// $("button").on("click", function(){
+//     $("div").slideUp(500, function(){
+//         console.log("Slide Completed");
+//     });
+    
+// });
+
+$("button").on("click", function(){
+    $("div").slideToggle(500, function(){
+        console.log("Slide Completed");
+    });
+    
+});
+
