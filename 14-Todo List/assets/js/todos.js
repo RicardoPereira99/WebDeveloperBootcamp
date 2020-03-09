77f67b69eec56a80127c57ac03705b8a559a5ce6
Ricardo Pereira <ricardojfpereira99@gmail.com>
2020-03-09 11:46:49 +0000

    Deleting Todo

--- 14-Todo List/assets/js/todos.js
+++ 14-Todo List/assets/js/todos.js
@@ -1,9 +1,22 @@
-// Check Off Specific Todos By Clicking
-
-
 //textDecoration: "line-through" property must be camelCase because of in javascript we can't use '-'
+
+
+// Check Off Specific Todos By Clicking
 
 $("li").click(function () {
 
     $(this).toggleClass("completed");
 });
+
+//Click on X to delete To-do
+
+$("span").click(function(event) {
+   
+    $(this).parent().fadeOut(500, function() {
+
+        $(this).remove();
+    });
+
+    event.stopPropagation();
+});
+
