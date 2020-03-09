cd5fb46cdafeefb224397c832c7d65d1dec1cdde
Ricardo Pereira <ricardojfpereira99@gmail.com>
2020-03-09 12:05:33 +0000

    Creating Todos

--- 14-Todo List/assets/js/todos.js
+++ 14-Todo List/assets/js/todos.js
@@ -3,14 +3,14 @@
 
 // Check Off Specific Todos By Clicking
 
-$("li").click(function () {
+$("ul").on("click", "li" ,  function () {
 
     $(this).toggleClass("completed");
 });
 
 //Click on X to delete To-do
 
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
    
     $(this).parent().fadeOut(500, function() {
 
@@ -18,5 +18,18 @@ $("span").click(function(event) {
     });
 
     event.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function(event) {
+    
+    if (event.which === 13) {
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+
 });
 
